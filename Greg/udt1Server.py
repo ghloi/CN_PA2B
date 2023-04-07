22bb202482a0e42012d721ccae8cd36a1a780489
eduardoM <jematanevar@miners.utep.edu>
2023-04-07 15:17:03 -0600

    Fixed a minor change

--- Greg/udt1Server.py
+++ Greg/udt1Server.py
@@ -154,7 +154,7 @@ def send_snw(inputPort, clientIP, clientPort, timeout):
             dataPacket = packet.make(seqNum, dataChunk)
 
             #Send it
-            sock.send(dataPacket, sock, clientAddress)
+            udt.send(dataPacket, sock, clientAddress)
 
             #NOW WE TIMEOUT AND RECEIVE
             timerObj.start() #Start the timer
