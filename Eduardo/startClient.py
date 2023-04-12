428fe1283a6a4b28041a240093933331a4e2c7e7
eduardoM <jematanevar@miners.utep.edu>
2023-04-11 21:31:38 -0600

    Commented and added Final Report file

--- Eduardo/startClient.py
+++ Eduardo/startClient.py
@@ -1,9 +1,11 @@
 from client import *
+#Ask for important data 
 serverIp=input('Enter the server IP address: ')
 serverPort=int(input('Enter the server port number: '))
 protocol=input('Enter protocol: ')
 fileName=input('Enter file name: ')
 
+#Create object of Client class and start process. 
 clientStart=Client(serverIp, serverPort, protocol, fileName)
 
 clientStart.recieve()
